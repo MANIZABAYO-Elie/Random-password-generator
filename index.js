9ea3764a29b6f8b95faac759cb6ffd268c5eb438
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-11-11 07:31:14 +0200

    file committed

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function generatePassword(length, includeLowerCase, includeUpperCase, includeSymbols, includeNumbers){
+     
+     const lowerChars ="abcdefghijklmnopqrstuvwxyz"
+     const upperChars ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+     const numbers   = "1234567890"
+     const symbols =   "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+     let allowedChars = ""
+     let password = ""
+     return ""
+
+}
